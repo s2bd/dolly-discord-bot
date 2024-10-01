a1e0cba4982dfe20f1fa0b5cb8ca9d88585c5b50
Dewan Mukto <dewanmukto@gmail.com>
2024-10-01 18:03:13 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    TOKEN: "",
+    PREFIX: "",
+    LEAVE_IDLE: true,
+    LEAVE_IDLE_COUNTDOWN: 60,
+    LEAVE_FINISH: false,
+    LEAVE_STOP: false,
+    WEBHOOK_ID: 0,
+    WEBHOOK_TOKEN: "",
+}
